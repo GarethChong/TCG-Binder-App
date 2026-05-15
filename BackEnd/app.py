3c64b97b9435f2e49ca6637c4608f929fd294045
Gareth <garethkh.chong@gmail.com>
2026-05-15 17:20:46 +0800

    added register and login routes to authenticate users

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,16 +1,28 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
+from flask_bcrypt import Bcrypt
 
 #Flask knows which module to look for resources such as templates and static files, and it also allows the application to be run directly from this file.
 app = Flask(__name__)
 #dictionary that holds the configuration settings for the Flask application, specifically the URI for the SQLite database. 
 #tells SQLAlchemy where to find the database file (cards.db) that will be used to store and manage the card data.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cards.db'
+app.config['SECRET_KEY'] = 'hello i like cards'
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
 class Binder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Page(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -48,6 +60,44 @@ def get_binders():
     binders = Binder.query.all()
     binder_list = [{'id': binder.id, 'name': binder.name} for binder in binders]
     return jsonify(binder_list)
+
+@app.route('/register', methods=['POST'])
+def register():
+    #request for a username and password to create a new user account
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    #checks if the username already exists
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'Username already exists!'}), 400
+
+    #creates a new user and adds them to the database
+    new_user = User(username=username, password=bcrypt.generate_password_hash(password).decode('utf-8'))
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully!'})
+
+#POST used for security reasons to prevent sensitive information from being exposed in the URL
+@app.route('/login', methods=['POST'])
+def login():
+    #request for a username and password to create a new user account
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+
+    #checks if the username exists
+    if not user:
+        return jsonify({'message': 'User does not exist, please create a new account'}), 400
+
+    #checks if the password is correct
+    if bcrypt.check_password_hash(user.password, password):
+        login_user(user)
+        return jsonify({'message': 'User logged in successfully!'})
+    else:
+        return jsonify({'message': 'Incorrect password!'}), 400
 
 #application context allows full access to the database and its tables, ensuring that the necessary resources are available when creating the tables.
 with app.app_context():
