8e24521173f9406af26b88380732f7d47fa25758
Gareth <garethkh.chong@gmail.com>
2026-05-15 16:07:41 +0800

    added new route /binderlist

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+#Flask knows which module to look for resources such as templates and static files, and it also allows the application to be run directly from this file.
 app = Flask(__name__)
+#dictionary that holds the configuration settings for the Flask application, specifically the URI for the SQLite database. 
+#tells SQLAlchemy where to find the database file (cards.db) that will be used to store and manage the card data.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cards.db'
 db = SQLAlchemy(app)
-
-@app.route('/')
-def home():
-    return "Welcome to the TCG Binder App!"
-
 
 class Binder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,8 +26,24 @@ class Card(db.Model):
     image_url = db.Column(db.String(500))
     page_id = db.Column(db.Integer, db.ForeignKey('page.id'))
 
+@app.route('/')
+def home():
+    return "Welcome to the TCG Binder App!"
+
+@app.route('/binderlist', methods=['POST'])
+def binder_list():
+    data = request.get_json()
+    name = data.get('name')
+
+    new_binder = Binder(name=name)
+    db.session.add(new_binder)
+    db.session.commit()
+    return jsonify({'message': 'Binder created successfully!'})
+
+#application context allows full access to the database and its tables, ensuring that the necessary resources are available when creating the tables.
 with app.app_context():
     db.create_all()
 
+#only run the app if this file is executed directly, preventing it from running when imported as a module in other parts of the application.
 if __name__ == '__main__':
     app.run(debug=True)
