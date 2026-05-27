7fc460abff1e41d4519c3a5a75b90e2c73aa90e3
Gareth <garethkh.chong@gmail.com>
2026-05-27 14:55:55 +0800

    added option to view list of binders in binders to frontend

--- frontend/src/pages/BinderList.jsx
+++ frontend/src/pages/BinderList.jsx
@@ -1,26 +1,61 @@
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function BinderList() {
+    const [binders, setBinders] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
     const navigate = useNavigate()
 
-    const logout = async () => {
-            const response = await fetch('http://localhost:5000/logout', {
-                method: 'POST',
-                credentials: 'include', //essential for flask login sessions
-            })
-            const data = await response.json()
+    useEffect(() => {
+        getBinders()
+    }, [])
 
-            if (response.ok) {
-                navigate('/login')
+    const getBinders = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/binderlist', {
+                method: 'GET',
+                credentials: 'include',
+            })
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch binders')
             }
+
+            const data = await response.json()
+            setBinders(data)
+        } catch (err) {
+            setError(true)
+        } finally {
+            setLoading(false)
+        }
     }
 
-    return (    
-        <div>
-            <h1>Binder List</h1>
-             <button onClick={() => logout()}>Logout</button>
-        </div>
-    )                
+    const logout = async () => {
+        const response = await fetch('http://localhost:5000/logout', {
+            method: 'POST',
+            credentials: 'include',
+        })
+        const data = await response.json()
+
+        if (response.ok) {
+            navigate('/login')
+        }
+    }
+
+    return loading
+        ? <p>Loading...</p>
+        : error
+            ? <p>Sorry, could not fetch Binders</p>
+            : <div>
+                <h1>
+                    Binder List
+                </h1>
+                {binders.map(binder => (
+                    <p key={binder.id}>{binder.name}</p>
+                ))}
+                <button onClick={() => logout()}>Logout</button>
+            </div>
 }
 
 export default BinderList
